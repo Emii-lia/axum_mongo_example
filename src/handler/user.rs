8e741a0ccc492aaefcead574796a34b28c7df9ea
Emii-lia <fiaromiangaly@gmail.com>
2026-03-11 14:07:08 +0300

    refactor(handlers): adapting mongodb query usages based on the new update

--- src/handler/user.rs
+++ src/handler/user.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::Json;
-use mongodb::bson::{doc, oid::ObjectId};
+use mongodb::bson::{doc, oid::ObjectId, Document};
 use mongodb::Collection;
 use crate::dto::user::{CreateUserDto, UpdateUserDto, UserResponseDto};
 use crate::model::user::User;
@@ -25,7 +25,7 @@ pub async fn create_user(
   State(state): State<Arc<AppState>>,
   Json(payload): Json<CreateUserDto>
 ) -> Result<(StatusCode, Json<UserResponseDto>), (StatusCode, String)> {
-  let collection = state.db.collection("users");
+  let collection: Collection<User> = state.db.collection("users");
 
   let new_user = User {
     id: None,
@@ -33,11 +33,11 @@ pub async fn create_user(
     email: payload.email,
   };
 
-  match collection.insert_one(new_user, None).await {
+  match collection.insert_one(new_user).await {
     Ok(result) => {
       let user_id = result.inserted_id.as_object_id().unwrap();
       let created_user = collection
-        .find_one(doc! { "_id": user_id}, None)
+        .find_one(doc! { "_id": user_id})
         .await
         .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to fetch user".to_string()))?
         .ok_or((StatusCode::NOT_FOUND, "User not found".to_string()))?;
@@ -68,9 +68,9 @@ pub async fn create_user(
 pub async fn list_users(
   State(state): State<Arc<AppState>>,
 ) -> Result<Json<Vec<UserResponseDto>>, (StatusCode, String)> {
-  let collection = state.db.collection("users");
+  let collection: Collection<User> = state.db.collection("users");
 
-  match collection.find(None, None).await {
+  match collection.find(Document::default()).await {
     Ok(cursor) => {
       let docs: Vec<User> = cursor.try_collect()
         .await
@@ -110,7 +110,7 @@ pub async fn get_user_by_id(
   let collection: Collection<User> = state.db.collection("users");
   let object_id = ObjectId::parse_str(&id).map_err(|_| (StatusCode::BAD_REQUEST, "Invalid ObjectId".to_string()))?;
 
-  match collection.find_one(doc! { "_id": object_id }, None).await {
+  match collection.find_one(doc! { "_id": object_id }).await {
     Ok(Some(user)) => {
       Ok(Json(UserResponseDto {
         id: user.id.unwrap().to_string(),
@@ -146,7 +146,7 @@ pub async fn update_user(
   let collection: Collection<User> = state.db.collection("users");
   let object_id = ObjectId::parse_str(&id).map_err(|_| (StatusCode::BAD_REQUEST, "Invalid ObjectId".to_string()))?;
   
-  let existing_user = collection.find_one(doc! { "_id": object_id }, None).await
+  let existing_user = collection.find_one(doc! { "_id": object_id }).await
     .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to fetch user".to_string()))?
     .ok_or((StatusCode::NOT_FOUND, "User not found".to_string()))?;
   
@@ -157,7 +157,7 @@ pub async fn update_user(
     }
   };
   
-  match collection.find_one_and_update(doc! { "_id": object_id }, update, None).await {
+  match collection.find_one_and_update(doc! { "_id": object_id }, update).await {
     Ok(Some(updated_user)) => Ok(Json(UserResponseDto {
       id: updated_user.id.unwrap().to_string(),
       name: updated_user.name,
@@ -188,7 +188,7 @@ pub async fn delete_user(
 ) -> Result<StatusCode, (StatusCode, String)> {
   let collection: Collection<User> = state.db.collection("users");
   let object_id = ObjectId::parse_str(&id).map_err(|_| (StatusCode::BAD_REQUEST, "Invalid ObjectId".to_string()))?;
-  match collection.delete_one(doc! { "_id": object_id }, None).await {
+  match collection.delete_one(doc! { "_id": object_id }).await {
     Ok(result) if result.deleted_count == 1 => Ok(StatusCode::NO_CONTENT),
     Ok(_) => Err((StatusCode::NOT_FOUND, "User not found".to_string())),
     Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to delete user".to_string()))
